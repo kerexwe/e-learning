870511580e44c635f8aa63df2cc42a318a54a039
Rami-0 <Ramysh11@icloud.com>
2023-05-25 03:41:27 +0600

    feat: weWorkWith and traslation + fix: class & css  mistaks

--- src/pages/CoursesLanding/components/Main/Main.tsx
+++ src/pages/CoursesLanding/components/Main/Main.tsx
@@ -4,40 +4,44 @@ import css from './Main.module.scss';
 import svg from './assets/main.svg';
 import arrow from './assets/Arrow-main.svg';
 import play from './assets/play.svg';
-import { t } from 'i18next';
+import { useTranslation } from 'react-i18next';
+
+const base = "Landing.Main."
 
 const Main: FC = () => {
+	const { t } = useTranslation();
+
 	return (
 		<section className={`${css.section} container`}>
 			<div className={css.content}>
 				<h1>
-					Free <span className={css.gray}>Online</span> <span className={css.orange}>Courses</span> From The Experts
+				{t(`${base}free`)} <span className={css.gray}>{t(`${base}Online`)}</span> <span className={css.orange}>{t(`${base}Courses`)}</span> {t(`${base}From The Experts`)} 
 				</h1>
 				<p className='secondary_mid_text'>Etiam sollicitudin, ipsum eu pulvinar rutrum, tellus ipsum laoreet sapien, quis venenatis ante odio sit amet eros. </p>
 				<span className={css.buttons}>
 					<button>
-						{t('Landing.Main.Button')} <img src={arrow} alt='arrow' />
+						{t(`${base}Button`)} <img src={arrow} alt='arrow' />
 					</button>
 					<h6>
 						<button>
 							<img src={play} alt='play' />
 						</button>
-						Watch Video
+						{t(`${base}WatchVideo`)}
 					</h6>
 				</span>
 				<hr />
 				<div className={css.info}>
                     <span>
                         <h3>14k+</h3>
-                        <p className='secondary_mid_text'>Learners</p>
+                        <p className='secondary_mid_text'>{t(`${base}Learners`)}</p>
                     </span>
                     <span>
                         <h3>1,05k+</h3>
-                        <p className='secondary_mid_text'>Courses</p>
+                        <p className='secondary_mid_text'>{t(`${base}Courses_1`)}</p>
                     </span>
                     <span>
                         <h3>59k+</h3>
-                        <p className='secondary_mid_text'>Graduates</p>
+                        <p className='secondary_mid_text'>{t(`${base}Graduates`)}</p>
                     </span>
                 </div>
 			</div>
