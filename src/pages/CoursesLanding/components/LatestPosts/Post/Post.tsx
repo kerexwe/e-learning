7a7e1f924b4906e534546e99249fbe890ad3857b
Rami-0 <Ramysh11@icloud.com>
2023-05-27 23:35:24 +0600

    feat: adaptive + translation

--- src/pages/CoursesLanding/components/LatestPosts/Post/Post.tsx
+++ src/pages/CoursesLanding/components/LatestPosts/Post/Post.tsx
@@ -2,12 +2,10 @@ import { FC } from 'react';
 import css from './Post.module.scss';
 import { PostType } from '../../../../../types/types';
 
-const Post: FC<PostType> = ({ img, month, day, h5, p, name, time, pfp }) => {
+export const Post: FC<PostType> = ({ img, month, day, h5, p, name, time, pfp }) => {
 	return (
 		<div className={css.card}>
-			<div className={css.card_img}>
-				<img src={img} alt='post_photo' />
-			</div>
+			<div className={css.card_img} style={{ background: `url(${img}) center/cover no-repeat` }}></div>
 			<article className={css.content}>
 				<div className={css.date}>
 					<h6 className='Subtitle2'>{month}</h6>
@@ -32,4 +30,18 @@ const Post: FC<PostType> = ({ img, month, day, h5, p, name, time, pfp }) => {
 	);
 };
 
-export default Post;
+export const Post_Small: FC<PostType> = ({ img, month, day, h5, p, name, time, pfp }) => {
+	return (
+		<>
+			<div className={css.card_small}>
+				<div className={css.card_img_small} style={{ background: `url(${img}) center/cover no-repeat` }}></div>
+				<div className={css.card_small__content}>
+					<h6>{h5}</h6>
+					<span>
+						<p style={{fontWeight: 400}} className='secondary_small_text'>{`${month} ${day}, 2023 • ${time}`}</p>
+					</span>
+				</div>
+			</div>
+		</>
+	);
+};
