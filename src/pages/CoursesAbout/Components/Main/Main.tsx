e03842d98e62524b4f73221718c29e5fca475784
Rami-0 <Ramysh11@icloud.com>
2023-06-04 03:49:38 +0600

    feat: translation

--- src/pages/CoursesAbout/Components/Main/Main.tsx
+++ src/pages/CoursesAbout/Components/Main/Main.tsx
@@ -3,16 +3,19 @@ import illustration from '../../assets/illustration_AboutUsPage.svg';
 import css from './Main.module.scss';
 import Button from './../../../../components/Button/Button';
 import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 const Main: FC = () => {
   const navigate = useNavigate()
+	const {t} = useTranslation()
+	const base = "AboutUsPage.Main."
 	return (
 		<section className={css['OnlineCoursesWrapper'] + " container"}>
 			<main className={css['OnlineCoursesWrapper_main']}>
 				<article>
-					<h1>Online Courses</h1>
+					<h1>{t(`${base}h1`)}</h1>
           <p className='secondary_mid_text mainColorText'>Nunc nulla. Ut leo. Pellentesque commodo eros a enim. Nunc egestas, augue at pellentesque laoreet, felis eros vehicula leo, at malesuada velit leo quis pede.</p>
 				</article>
-				<Button text={'Browse Courses'} type='orange' onClick={() => navigate('/Courses/list')} />
+				<Button text={t(`${base}button`)} type='orange' onClick={() => navigate('/Courses/list')} />
 			</main>
 			<aside className={css['OnlineCoursesWrapper_aside']}>
         <img src={illustration} alt="illustration" />
--- src/pages/CoursesAbout/Components/Static/Static.tsx
+++ src/pages/CoursesAbout/Components/Static/Static.tsx
@@ -2,32 +2,36 @@ import { FC } from 'react';
 import css from './Static.module.scss';
 import img from '../../assets/img_static.svg';
 import { AppSelector } from '../../../../redux';
+import { useTranslation } from 'react-i18next';
 
 const Static: FC = () => {
 	const themeDark = AppSelector((state) => state.themeDark);
+  const {t} = useTranslation()
+	const base = "AboutUsPage.Static."
+
 	return (
 		<section className={css['staticWrapper'] + ' container'}>
 			<div className={css['staticWrapper_top']}>
 				<p className='secondary_small_text OrangeText'>Nullam accumsan lorem in dui.</p>
 				<article>
-					<h3>Effective Forms Advertising Internet Web Site</h3>
-					<p className='secondary_mid_text'>Over 92% of computers are infected with Adware and spyware. Such software is rarely accompanied by uninstall utility and even when it is it almost always leaves broken Windows</p>
+					<h3>{t(`${base}h3`)}</h3>
+					<p className='secondary_mid_text'>{t(`${base}p`)}</p>
 				</article>
 			</div>
 			<main className={css['staticWrapper_main']}>
 				<div className={css['main_cards']}>
 					<span className={css['card']}>
-						<h4 className={themeDark ? css['card_tag--DarkGray'] : css['card_tag--gray']}>Learners</h4>
+						<h4 className={themeDark ? css['card_tag--DarkGray'] : css['card_tag--gray']}>{t(`${base}cards.tag_first`)}</h4>
 						<h2>14k+</h2>
 						<p className='secondary_mid_text'>Ut varius tincidunt libero. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.</p>
 					</span>
 					<span className={css['card']}>
-						<h4 className={themeDark ? css['card_tag--DarkGray'] : css['card_tag--gray']}>Courses</h4>
+						<h4 className={themeDark ? css['card_tag--DarkGray'] : css['card_tag--gray']}>{t(`${base}cards.tag_sec`)}</h4>
 						<h2>1.05k+</h2>
 						<p className='secondary_mid_text'>Ut varius tincidunt libero. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.</p>
 					</span>
 					<span className={css['card']}>
-						<h4 className={themeDark ? css['card_tag--DarkGray'] : css['card_tag--gray']}>Graduates</h4>
+						<h4 className={themeDark ? css['card_tag--DarkGray'] : css['card_tag--gray']}>{t(`${base}cards.tag_thd`)}</h4>
 						<h2>52k+</h2>
 						<p className='secondary_mid_text'>Ut varius tincidunt libero. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.</p>
 					</span>
